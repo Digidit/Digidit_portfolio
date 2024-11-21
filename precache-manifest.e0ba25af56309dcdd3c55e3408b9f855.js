03299c8ee2fadcf94b7ae74a54fe7e1a25224235
Dhiraj Badgujar <dhirajbadgujar095@gmail.com>
2024-11-21 21:12:32 +0530

    Updates

---
+++ precache-manifest.e0ba25af56309dcdd3c55e3408b9f855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f409f8ac6a3b614156bbd912a3074a05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a416bae525692840ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bee92e1d8e65b71051a4",
+    "url": "/static/js/2.3a6d1155.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "e3a416bae525692840ad",
+    "url": "/static/js/main.d8b394bd.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  }
+]);
