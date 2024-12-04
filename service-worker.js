17705fed8db6db73e51561c7e885a05e7c1b17ce
Dhiraj Badgujar <dhirajbadgujar095@gmail.com>
2024-12-04 22:00:12 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c34f42fb3318c8bcaa9d5e5da638ae9.js"
+  "/precache-manifest.7771692e8e383861aef5f2b4907766a6.js"
 );
 
 self.addEventListener('message', (event) => {
